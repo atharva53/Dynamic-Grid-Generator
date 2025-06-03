8293e06e138baebcc869b810cf5e8d2b4957230d
Atharva Mhetre <atharvamhetre53@gmail.com>
2025-06-03 17:29:58 +0530

    Remove the Button & OnClick() and Optimize the Code

--- README.md.txt
+++
@@ -1,3 +0,0 @@
-<h1>Dynamic Grid Generator<h1>
-<br><hr>
-<p>Enter Rows and Columns values, according to that dimensions the Grid is Generated </p>
--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd35eb010f75b48b9b0c578284544f2c1407349a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
